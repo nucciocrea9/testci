f1b824e16350803ff527d2c5f01fa40366f73a5c
nuccio <nuccio.nc96@gmail.com>
2021-09-24 11:44:19 +0200

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> this is a test about ci/cd</h1>
+        <h1> this is a test about ci/cd again</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
